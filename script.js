1e7e23c7e1a0df6b0b06586141dfbeaadc7e9ad3
karthik-126 <bk24csb0a12@student.nitw.ac.in>
2025-03-20 21:11:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,15 @@ form.addEventListener(`submit`, function(e){
     const results = document.querySelector(`#results`)
 let bmi=0;
    if (height =='' || height<0 || height == NaN) {
-    // results me value add karne ke liye innerHTML ka use karte hai
     results.innerHTML = `please give a valid height ${height}`;
     
    }
    if (weight == '' || weight<0 || weight == NaN) {
-    // results me value add karne ke liye innerHTML ka use karte hai
     results.innerHTML = `please give a valid weight ${weight}`;
     
    }
    else{
      bmi = (weight/ ((height*height)/ 10000)).toFixed(2);
-    //show the results
     results.innerHTML = `<span>${bmi}</span>`;
    }
    if(bmi< 18.6 ){
